1ef61036ad9c81ce51cd1a6e54493a04475078ae
Michael Yosavani Putra <surbaktievan@ymail.com>
2018-09-29 17:54:54 +0700

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Menu, Container } from "semantic-ui-react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
+import About from "./pages/about";
+import Work from "./pages/work";
+
+export default class App extends Component {
+  state = { activeItem: "about" };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
+    const { activeItem } = this.state;
+
     return (
-      <div>
-        <h1>evan</h1>
-      </div>
+      <Router>
+        <div>
+          <Container>
+            <div>
+              <Menu secondary size="massive">
+                <Menu.Menu position="right">
+                  <Menu.Item
+                    as={Link}
+                    to="/"
+                    name="About"
+                    active={activeItem === "about"}
+                    onClick={this.handleItemClick}
+                  />
+                  <Menu.Item
+                    as={Link}
+                    to="/work"
+                    name="Work"
+                    active={activeItem === "work"}
+                    onClick={this.handleItemClick}
+                  />
+                </Menu.Menu>
+              </Menu>
+            </div>
+          </Container>
+          <Route exact path="/" component={About} />
+          <Route path="/work" component={Work} />
+        </div>
+      </Router>
     );
   }
 }
-
-export default App;
---
+++ src/pages/work.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+
+class Work extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Container>
+        <h1>Work page</h1>
+      </Container>
+    );
+  }
+}
+
+export default Work;
