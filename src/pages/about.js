74c5ae23776e5fac7dbcf5736d5e20c8442b85a1
Michael Yosavani Putra <surbaktievan@ymail.com>
2018-10-03 11:23:13 +0700

    fixing margin bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -28,7 +28,7 @@ class About extends Component {
               <h1 className="judul-about">I'm Evan</h1>
             </div>
             <div className="judul1">
-              <h3 className="judul-about1">a FullStack Web Developer</h3>
+              <h3 className="judul2">a FullStack Web Developer</h3>
             </div>
           </Container>
         </Segment>
@@ -44,6 +44,18 @@ class About extends Component {
             and love to create for web and mobile devices.
           </div>
           <div className="bio2">What i can do.</div>
+          <div className="bio3">
+            <div className="bio4">Front-End</div>
+            <div>
+              <div className="bio5">Technology that I used to use</div>
+              <div className="bio6">
+                HTML <br />
+                CSS <br />
+                JavaScript <br />
+                React
+              </div>
+            </div>
+          </div>
         </Container>
         <Footer />
       </div>
